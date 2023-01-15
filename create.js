f8c834f35ab16857c64aeff521e7f6dfe8879f97
ZbWeR <1641064392@qq.com>
2023-01-15 21:19:56 +0800

    feat: 实现ajax,load动画与button禁用

--- create.js
+++ create.js
@@ -1,9 +1,9 @@
-let lapi = ['https://api.gmit.vip/Api/DmImg?format=json', 'https://moe.jitsu.top/img/?sort=r18&size=original&type=json'];
+var lapi = ['https://api.gmit.vip/Api/DmImg?format=json', 'https://moe.jitsu.top/img/?sort=r18&size=original&type=json'];
 
 function ajaxFetch(pos) {
     const tarImg = document.querySelector('.content .pic img');
     const url = lapi[pos];
-    // console.log(url);
+    console.log("running");
     fetch(url)
         .then(response => {
             if (response.ok) {
@@ -16,10 +16,26 @@ function ajaxFetch(pos) {
             if (pos == 0) resUrl = data.data.url;
             else resUrl = data.pics[0];
             tarImg.src = resUrl;
+            console.log(resUrl);
         })
 }
 
-function create() {
+async function create() {
+    console.log("如果你懂得取消disable,请不要重复点击按钮,api就在源码里自己拿");
+    var loading = document.querySelector('.content .pic .loader');
+    document.querySelector('.content .header button').disabled = true;
     const pos = Math.floor(Math.random() * lapi.length);
-    ajaxFetch(pos);
+    // ajaxFetch(pos);
+    loading.classList.remove('hidden');
+    await ajaxFetch(pos);
+    document.querySelector('.content .pic img').onload = () => {
+        loading.classList.add('hidden');
+        document.querySelector('.content .header button').disabled = false;
+    }
+    // setTimeout(() => {
+    //     console.log("@@@");
+    //     loading.classList.add('hidden');
+    // }, 1500);
+    // loading.classList.add('hidden');
+    // ajaxFetch(pos);
 }
