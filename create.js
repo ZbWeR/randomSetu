07b4b31388381ebe5c19f0ccb3d6016273b1dd8b
ZbWeR <1641064392@qq.com>
2023-01-15 23:19:28 +0800

    🐞 fix: 加入超时错误信息处理

--- create.js
+++ create.js
@@ -1,4 +1,6 @@
 var lapi = ['https://api.gmit.vip/Api/DmImg?format=json', 'https://moe.jitsu.top/img/?sort=r18&size=original&type=json'];
+var loading = document.querySelector('.content .pic .loader');
+var btn = document.querySelectorAll('.content .header button');
 
 function ajaxFetch(pos) {
     const tarImg = document.querySelector('.content .pic img');
@@ -6,7 +8,6 @@ function ajaxFetch(pos) {
     console.log("running");
     fetch(url)
         .then(response => {
-            // console.log("2333");
             if (response.ok) {
                 return response.json();
             }
@@ -19,17 +20,26 @@ function ajaxFetch(pos) {
             tarImg.src = resUrl;
             console.log(resUrl);
         })
+        .catch(err => {
+            console.log(err);
+            setTimeout(() => hiddenAll(), 200);
+            btn[1].innerHTML = "魔法上网";
+            btn[1].disabled = true;
+        })
+
 }
 
 async function create(pos) {
     console.log("如果你懂得取消disable,请不要重复点击按钮,api就在源码里自己拿");
-    var loading = document.querySelector('.content .pic .loader');
-    let btn = document.querySelectorAll('.content .header button');
     for (let i = 0; i < btn.length; i++) btn[i].disabled = true;
     loading.classList.remove('hidden');
     await ajaxFetch(pos);
-    document.querySelector('.content .pic img').onload = () => {
-        loading.classList.add('hidden');
-        for (let i = 0; i < btn.length; i++) btn[i].disabled = false;
-    }
+    document.querySelector('.content .pic img').onload = () => hiddenAll();
+    // document.querySelector('.content .pic img').onload = hiddenAll();
+}
+
+function hiddenAll() {
+    console.log("@222");
+    for (let i = 0; i < btn.length; i++) btn[i].disabled = false;
+    loading.classList.add('hidden');
 }
