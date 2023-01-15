d90a658ac673dd2f8ec6a505923ab8c6e2c81ab8
ZbWeR <1641064392@qq.com>
2023-01-15 22:47:29 +0800

    ✨ feat: 增加图片类型选择功能

--- create.js
+++ create.js
@@ -6,6 +6,7 @@ function ajaxFetch(pos) {
     console.log("running");
     fetch(url)
         .then(response => {
+            // console.log("2333");
             if (response.ok) {
                 return response.json();
             }
@@ -20,16 +21,15 @@ function ajaxFetch(pos) {
         })
 }
 
-async function create() {
+async function create(pos) {
     console.log("如果你懂得取消disable,请不要重复点击按钮,api就在源码里自己拿");
     var loading = document.querySelector('.content .pic .loader');
-    document.querySelector('.content .header button').disabled = true;
-    const pos = Math.floor(Math.random() * lapi.length);
-    // ajaxFetch(pos);
+    let btn = document.querySelectorAll('.content .header button');
+    for (let i = 0; i < btn.length; i++) btn[i].disabled = true;
     loading.classList.remove('hidden');
     await ajaxFetch(pos);
     document.querySelector('.content .pic img').onload = () => {
         loading.classList.add('hidden');
-        document.querySelector('.content .header button').disabled = false;
+        for (let i = 0; i < btn.length; i++) btn[i].disabled = false;
     }
 }
